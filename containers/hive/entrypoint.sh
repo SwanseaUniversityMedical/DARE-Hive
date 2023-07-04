ffc773586674cf093331943facb8138ff3637146
Alex <a.s.lee@swansea.ac.uk>
2023-07-04 10:14:17 +0100

    feat: update version numbers for chart, fix dangerous behaviour in values, pin env vars in Dockerfile where appropriate

--- containers/hive/entrypoint.sh
+++ containers/hive/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION}
-export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.12.262.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-${HADOOP_VERSION}.jar
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
 
 echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on 5432 ..."
