bb6f512122f9214efd55d85241842814da0b24c3
Alex <a.s.lee@swansea.ac.uk>
2024-03-18 11:25:50 +0000

    feat(container): don't hardcode mysql type or port, add postgres driver

--- containers/hive/entrypoint.sh
+++ containers/hive/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
+export METASTORE_DB_PORT=${METASTORE_DB_PORT:-3306}
+export METASTORE_DB_TYPE=${METASTORE_DB_TYPE:-mysql}
 
-echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on 3306 ..."
+echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on ${METASTORE_DB_PORT} ..."
 
-while ! nc -z ${METASTORE_DB_HOSTNAME} 3306; do
+while ! nc -z ${METASTORE_DB_HOSTNAME} ${METASTORE_DB_PORT}; do
   sleep 1
 done
 
-echo "Database on ${METASTORE_DB_HOSTNAME}:3306 started"
-echo "Init apache hive metastore on ${METASTORE_DB_HOSTNAME}:3306"
+echo "Database on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT} started"
+echo "Init apache hive metastore on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT}"
 
-/opt/apache-hive-metastore-3.1.3-bin/bin/schematool -initSchema -dbType mysql
+/opt/apache-hive-metastore-3.1.3-bin/bin/schematool -initSchema -dbType ${METASTORE_DB_TYPE}
 /opt/apache-hive-metastore-3.1.3-bin/bin/start-metastore
